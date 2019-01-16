4c786fde9d68cbe9e9a600a5abd0bdb8dedeeba3
Mac <mac@MacBook-Pro.fios-router.home>
2019-01-16 04:46:11 -0600

    fix update issue, should return latest record

--- src/graphql/_lib/mongoCRUD.js
+++ src/graphql/_lib/mongoCRUD.js
@@ -1,33 +1,35 @@
 const mongoCRUD = {
   save,
-  remove
-}
+  remove,
+};
 
+// add when id is undefined, else update when id is defined
 function save(Entity, recordName) {
   return (parent, args, context, info) => {
     if (args.id === undefined) {
       let record = new Entity(args);
-      return record.save().then( data => {
+      return record.save().then(data => {
         console.log(`Added ${recordName} with id=${data._id}.`);
         return data;
       });
     } else {
-      return Entity.findOneAndUpdate({ _id: args.id }, args).then( data => {
+      return Entity.findOneAndUpdate({ _id: args.id }, args, {
+        new: true,
+      }).then(data => {
         console.log(`Updated ${recordName} with id=${data._id}.`);
         return data;
       });
     }
   };
-};
+}
 
 function remove(Entity, recordName) {
-    return (parent, args, context, info) => {
-   
-    return Entity.findOneAndRemove({ _id: args.id }).then(data => {   
+  return (parent, args, context, info) => {
+    return Entity.findOneAndRemove({ _id: args.id }).then(data => {
       console.log(`Deleted ${recordName} with id=${data._id}.`);
       return data;
     });
-  }
-};
+  };
+}
 
 module.exports = mongoCRUD;
