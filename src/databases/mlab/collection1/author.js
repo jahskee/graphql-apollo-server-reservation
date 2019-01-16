3235b46ae3acdf79129e339f72eba2b807b3689d
Mac <mac@MacBook-Pro.fios-router.home>
2019-01-16 06:01:04 -0600

    few more fixes

--- src/databases/mlab/collection1/author.js
+++ src/databases/mlab/collection1/author.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const authorSchema = new Schema({
+/*---- Author ----*/
+const schema = new Schema({
     name: String,
     age: Number,
-    authorId: String,
 });
 
-module.exports = mongoose.model('Author', authorSchema);
+module.exports = mongoose.model('Author', schema);
--- src/databases/mlab/collection1/book.js
+++ src/databases/mlab/collection1/book.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
+/*---- Book ----*/
+const schema = new Schema({
     name: String,
     genre: String,
     authorId: String,
 });
 
-module.exports = mongoose.model('Book', bookSchema);
+module.exports = mongoose.model('Book', schema);
